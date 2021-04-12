a35565f9902645de028d2f38ee3b952dd0f87421
Jaron <jaron.grigat@googlemail.com>
2021-04-12 12:57:20 +0200

    added scale plugin

---
+++ scale.py
@@ -0,0 +1,10 @@
+from Doberman import SerialSensor, utils
+import re
+
+class scale(SerialSensor):
+    """
+    Large scale. should update to right name for pancake. but now quick implementation for XeBra.
+    """
+
+    def SetParameters(self):
+        self.reading_pattern = re.compile((f'(?P<value>{utils.number_regex})kg').encode())
