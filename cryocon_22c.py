4c7e09b780964bb4f2a4d99cacdf8bd00bd9072f
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2023-03-02 10:24:28 +0100

    Cryocon fixed (#5)
    Added the eol character

--- cryocon_22c.py
+++ cryocon_22c.py
@@ -10,23 +10,22 @@ class cryocon_22c(LANDevice):
         'set setpoint <value>: change the setpoint',
     ]
 
-    def set_parameters(self):
-        self._msg_end = ';\n'
-        self.commands = {  # these are not case sensitive
-            'identify': '*idn?',
-            'getTempA': 'input? a:units k',
-            'getTempB': 'input? b:units k',
-            'getSP1': 'loop 1:setpt?',
-            'getSP2': 'loop 2:setpt?',
-            'getLp1Pwr': 'loop 1:htrread?',
-            'getLp2Pwr': 'loop 2:htrread?',
-            'setTempAUnits': 'input a:units k',
-            'settempBUnits': 'input b:units k',
-            'setSP': 'loop {ch}:setpt {value}',
-        }
-        self.value_pattern = re.compile(('(?P<value>%s)' % utils.number_regex).encode())
-        self.command_patterns = [
-            (re.compile(r'setpoint (?P<ch>1|2) (?P<value>%s)' % utils.number_regex),
-             lambda x: self.commands['setSP'].format(**x.groupdict())),
-
-        ]
+    eol = b'\n'
+    _msg_end = ';\n'
+    commands = {  # these are not case sensitive
+        'identify': '*idn?',
+        'getTempA': 'input? a:units k',
+        'getTempB': 'input? b:units k',
+        'getSP1': 'loop 1:setpt?',
+        'getSP2': 'loop 2:setpt?',
+        'getLp1Pwr': 'loop 1:htrread?',
+        'getLp2Pwr': 'loop 2:htrread?',
+        'setTempAUnits': 'input a:units k',
+        'settempBUnits': 'input b:units k',
+        'setSP': 'loop {ch}:setpt {value}',
+    }
+    value_pattern = re.compile(('(?P<value>%s)' % utils.number_regex).encode())
+    command_patterns = [
+        (re.compile(r'setpoint (?P<ch>1|2) (?P<value>%s)' % utils.number_regex),
+        lambda x: self.commands['setSP'].format(**x.groupdict())),
+    ]
