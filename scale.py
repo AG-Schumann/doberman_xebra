38b72fc08af1b5931d1ff9767376bbd1d902132b
Tiffany Luce <115090963+TiffanyLuce@users.noreply.github.com>
2023-04-03 13:16:49 +0200

    negative values reading

--- scale.py
+++ scale.py
@@ -1,10 +1,16 @@
-from Doberman import SerialDevice, utils
+from Doberman import LANDevice, utils
 import re
 
-class scale(SerialDevice):
+class scale(LANDevice):
     """
     Large scale for XeBRA LN2 dewar
     """
+    eol = b'\r\x03'
 
-    def set_parameters(self):
-        self.value_pattern = re.compile((f'(?P<value>{utils.number_regex})kg').encode())
+    def process_one_value(self, name=None, data=None):
+        """
+        Takes the raw data as returned by send_recv and parses
+        it for the float. Only for the scales.
+        """
+        data = re.sub('\s','',data.decode())
+        return float(re.search('(?P<value>\-?[0-9]+(?:\.[0-9]+)?)kg', data).group('value'))
