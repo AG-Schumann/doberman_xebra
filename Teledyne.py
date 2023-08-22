c2a6335c6a19e9463d52924a3ddf511928590c2d
Tiffany Luce <tiffany.luce@physik.uni-freiburg.de>
2023-08-22 09:43:22 +0000

    works

--- Teledyne.py
+++ Teledyne.py
@@ -1,8 +1,8 @@
-from Doberman import SerialDevice, utils
+from Doberman import LANDevice, utils
 import re  # EVERYBODY STAND BACK xkcd.com/208
 
 
-class Teledyne(SerialDevice):
+class Teledyne(LANDevice):
     """
     Teledyne flow controller
     THCD-100
@@ -27,8 +27,8 @@ class Teledyne(SerialDevice):
         self.value_pattern = re.compile(('READ: *(?P<value>%s)' %
                                             utils.number_regex).encode())
         self.get_addr = re.compile(b'ADDR: *(?P<addr>[a-z])')
-        self.command_echo = f'\\*{self.device_address}\\*:' + '{cmd} *;'
-        self.retcode = f'!{self.device_address}!(?P<retcode>[beow])!'
+        self.command_echo = f'\\*{self.params["device_address"]}\\*:' + '{cmd} *;'
+        self.retcode = f'!{self.params["device_address"]}!(?P<retcode>[beow])!'
 
         self.setpoint_map = {'auto' : 0, 'open' : 1, 'close' : 2}
         self.reading_commands = {'flow' : self.basecommand.format(
